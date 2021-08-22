d46ec6ed262cc8c965e63f94dc6417b34186b28e
psmyrdek <przemek.smyrdek@gmail.com>
2021-08-22 18:10:25 +0200

    Update content

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
 const {readFile, writeFile} = require('fs').promises;
 const {join} = require('path');
-const {orderBy} = require('lodash');
+const {orderBy} = require("lodash");
 
-const inputPath = join(__dirname, './data/tim-ferris-1.txt');
-const outputPath = join(__dirname, './out/tokens.json');
-const threshold = 10;
-const minLen = 3
-
-const blacklist = [];
+const inputPath = join(__dirname, './data/tim-ferris-1-it.txt');
+const outputPath = join(__dirname, './out/words-1.json');
 
 function isTokenAccepted(token) {
-    return !blacklist.includes(token) && token.length >= minLen;
-}
-
-function extractMeaningfulTokens(tokens) {
-    return Object.entries(tokens).reduce((prev, [key, value]) => {
-        if (value > threshold) {
-            prev.push({token: key, popularity: value})
-        }
-        return prev;
-    }, []);
-}
-
-function buildResult(meaningfulTokens) {
-    return orderBy(meaningfulTokens, ['popularity'], ['desc']);
+    const minLen = 4
+    const blacklist = ['ferriss', 'huberman', 'sono']
+    return token.length >= minLen && !blacklist.includes(token);
 }
 
 function generateTokens(fileContent) {
     const tokensCount = {};
     fileContent.split(' ').forEach(rawToken => {
-        const token = rawToken.toLowerCase().replace(/[,.\-;\r\n]/g, '');
+        const token = rawToken.toLowerCase().replace(/[,:.\-;\r\n]/g, '');
 
         if (isTokenAccepted(token)) {
             if (tokensCount[token]) {
@@ -42,15 +27,25 @@ function generateTokens(fileContent) {
     return tokensCount
 }
 
-(async () => {
+function extractMeaningfulTokens(tokens) {
+    const threshold = 15
+    return Object.entries(tokens).reduce((prev, [key, value]) => {
+        if (value > threshold) {
+            prev.push({token: key, popularity: value})
+        }
+        return prev;
+    }, []);
+}
 
+function buildResult(meaningfulTokens) {
+    return orderBy(meaningfulTokens, ['popularity'], ['desc']).map(obj => obj.token)
+}
+
+(async () => {
     const fileContent = await readFile(inputPath, { encoding: 'utf-8' });
     const tokens = generateTokens(fileContent);
-
     const meaningfulTokens = extractMeaningfulTokens(tokens);
-
     await writeFile(outputPath, JSON.stringify(buildResult(meaningfulTokens), null, 2), 'utf-8');
-
 })();
 
 
